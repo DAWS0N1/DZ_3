3c19eeba2fa1e321f06ea7acd78809974867a406
dawson <gevorgyan.ara@yandex.ru>
2021-07-04 13:15:14 +0300

    Fixed code

--- src/main/java/Arrays/ArraysTemplate.java
+++ src/main/java/Arrays/ArraysTemplate.java
@@ -1,27 +1,40 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArraysTemplate {
     public static void main(String[] args) {
         int[] array = new int[20];
-        int maxNegativeIndex = 0, minPozitiveIndex = 0, maxNegative = -11, minPozitive = 11;
+        int maxNegative = -11, minPozitive = 11;
+        List<Integer> maxNegativeIndexes = new ArrayList<Integer>(), minPozitiveIndexes = new ArrayList<Integer>();
 
         for (int i=0; i < array.length; i++) {
             array[i] = ((int)(Math.random() * 21) - 10);
-
             if (array[i] < 0 && array[i] > maxNegative){
                 maxNegative = array[i];
-                maxNegativeIndex = i;
+                maxNegativeIndexes.clear();
+                maxNegativeIndexes.add(i);
+            } else if (array[i] < 0 && array[i] == maxNegative) {
+                maxNegativeIndexes.add(i);
             } else if (array[i] > 0 && array[i] < minPozitive) {
                 minPozitive = array[i];
-                minPozitiveIndex = i;
+                minPozitiveIndexes.clear();
+                minPozitiveIndexes.add(i);
+            } else if (array[i] > 0 && array[i] == minPozitive) {
+                minPozitiveIndexes.add(i);
             }
         }
         System.out.println(java.util.Arrays.toString(array));
 
-        System.out.println("Максимальное отрицательное: [" + maxNegativeIndex + "] " + maxNegative + "; Минимальное положительное: [" + minPozitiveIndex + "] " + minPozitive);
+        System.out.println("Максимальное отрицательное: " + maxNegativeIndexes + " " + maxNegative + "; Минимальное положительное: " + minPozitiveIndexes + " " + minPozitive);
 
-        array[maxNegativeIndex] = minPozitive;
-        array[minPozitiveIndex] = maxNegative;
+        for (int i = 0; i < maxNegativeIndexes.size(); i++) {
+            array[maxNegativeIndexes.get(i)] = minPozitive;
+        }
+        for (int i = 0; i < minPozitiveIndexes.size(); i++) {
+            array[minPozitiveIndexes.get(i)] = maxNegative;
+        }
 
         System.out.println(java.util.Arrays.toString(array));
     }
